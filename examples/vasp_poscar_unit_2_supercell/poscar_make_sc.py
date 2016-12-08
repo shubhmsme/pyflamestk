87a7b1fcd75247a979f0c44997ecd2fec76a2ccf
shubhmsme <shubhampandey@ufl.edu>
2016-12-08 16:26:19 -0500

    i did some stuff

--- examples/vasp_poscar_unit_2_supercell/poscar_make_sc.py
+++ examples/vasp_poscar_unit_2_supercell/poscar_make_sc.py
@@ -2,6 +2,7 @@ import pyflamestk.vasp as vasp
 
 fname_poscar_in = 'POSCAR'
 fname_poscar_out = 'POSCAR.out'
+
 sc_parameters = [3,3,3]
 unitcell = vasp.Poscar()
 unitcell.read_file(fname_in=fname_poscar_in)
@@ -9,3 +10,10 @@ unitcell.read_file(fname_in=fname_poscar_in)
 supercell = vasp.make_super_cell(unitcell,sc_parameters)
 supercell.write_file(fname_out=fname_poscar_out)
 
+print(supercell.lattice_parameter)
+print(supercell.h_matrix)
+
+supercell.h1 = supercell.h1 * 2
+
+print(supercell.lattice_parameter)
+print(supercell.h_matrix)
